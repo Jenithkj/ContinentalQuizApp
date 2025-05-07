3a5f855edb93b12020f4ef024d9649fc4f5536ec
jenith <jenith@quintetsolutions.com>
2025-05-07 13:56:00 +0530

    Added top view.

---
+++ ContinentalQuizApp/Utils/ColorsUtils.swift
@@ -0,0 +1,13 @@
+//
+//  ColorsUtils.swift
+//  ContinentalQuizApp
+//
+//  Created by Jenith KJ on 07/05/25.
+//
+
+import Foundation
+import SwiftUI
+
+enum Colors {
+    static let orange_FF7043 = Color("orange_FF7043")
+}
